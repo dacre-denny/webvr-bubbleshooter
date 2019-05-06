4927cbf4588552a546f8c8289accc97a2ac23a8d
Dacre <enquiries@mooce.co.nz>
2019-05-06 19:15:47 +1200

    Added bubble factory

---
+++ src/bubble.ts
@@ -0,0 +1,10 @@
+export const enum Colors {
+  RED,
+  BLUE,
+  GREEN,
+  YELLOW
+}
+
+export class Bubble {
+  constructor(mesh: BABYLON.InstancedMesh, color: Colors) {}
+}
---
+++ src/bubbleFactory.ts
@@ -0,0 +1,53 @@
+import { Bubble, Colors } from "./bubble";
+
+export class BubbleFactory {
+  private bubbles: Map<Colors, BABYLON.Mesh>;
+  private count: number;
+
+  private create(scene: BABYLON.Scene) {
+    const bubbles = new Map<Colors, BABYLON.Mesh>();
+
+    const mapping = new Map<Colors, BABYLON.Color3>([
+      [Colors.RED, BABYLON.Color3.Red()],
+      [Colors.BLUE, BABYLON.Color3.Blue()],
+      [Colors.GREEN, BABYLON.Color3.Green()],
+      [Colors.YELLOW, BABYLON.Color3.Yellow()]
+    ]);
+
+    for (const [key, color] of mapping.entries()) {
+      const material = new BABYLON.StandardMaterial(
+        `bubble.material.${key}`,
+        scene
+      );
+      material.diffuseColor = color;
+
+      const mesh = BABYLON.MeshBuilder.CreateIcoSphere(
+        `bubble.mesh.${key}`,
+        { radius: 0.25, subdivisions: 1 },
+        scene
+      );
+
+      mesh.isVisible = false;
+      mesh.material = material;
+
+      bubbles.set(key, mesh);
+    }
+
+    return bubbles;
+  }
+
+  constructor(scene: BABYLON.Scene) {
+    this.count = 0;
+    this.bubbles = this.create(scene);
+  }
+
+  public createBubble(): Bubble {
+    const color = Math.floor(Math.random() * 4) as Colors;
+    const mesh = this.bubbles.get(color);
+
+    const instance = mesh.createInstance(`bubble.mesh.instance.${this.count}`);
+    this.count++;
+
+    return new Bubble(instance, color);
+  }
+}
